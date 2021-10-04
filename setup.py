bce3ca3085d38ee168966c0d4407cb60a9f003f4
turnerm <turnermon@gmail.com>
2021-10-04 07:52:11 +0200

    Fixed find packages

--- setup.py
+++ setup.py
@@ -2,4 +2,4 @@
 
 from setuptools import find_packages, setup
 
-setup(name="aatools", version="0.0.0", packages=find_packages("aatools"))
+setup(name="aatools", version="0.0.0", packages=find_packages())
