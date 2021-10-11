497d632616cb31b56e05b37a5258be6f38df37c5
turnerm <turnermon@gmail.com>
2021-10-11 15:09:59 +0200

    Added version dunder

--- aatoolbox/__init__.py
+++ aatoolbox/__init__.py
@@ -4,3 +4,6 @@ The ``aatoolbox`` library contains modules designed to assist with
 downloading and processing the data required for climate- and weather-related
 anticipatory action.
 """
+import pkg_resources
+
+__version__ = pkg_resources.get_distribution("aatoolbox").version
