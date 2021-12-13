e39b3f6f4774104ca44e9e5edcdae3e07a9890c7
Tinkaa <tinkavalentijn@gmail.com>
2021-12-13 17:24:31 +0100

    rename functions

--- tests/test_geoboundingbox.py
+++ tests/test_geoboundingbox.py
@@ -8,7 +8,7 @@ from aatoolbox.utils.geoboundingbox import (
 )
 
 
-def test_area_round_coords():
+def test_geoboundingbox_round_coords():
     """Test that coordinates are correctly rounded and offset."""
     area = GeographicBoundingBox(north=1.05, south=-2.2, east=3.6, west=-4)
     area.round_area_coords(offset_val=0.4)
@@ -18,13 +18,13 @@ def test_area_round_coords():
     assert area.west == -4.4
 
 
-def test_area_filename():
+def test_geoboundingbox_filename():
     """Test that correct file string is returned."""
     area = GeographicBoundingBox(north=1, south=-2, east=3, west=-4)
     assert area.get_filename_repr() == "Np1Sm2Ep3Wm4"
 
 
-def test_get_area_from_shape():
+def test_get_geoboundingbox_from_shape():
     """Test that getting the area from a polygon works as expected."""
     n, s, e, w = (1, -2, 3, -4)
     shape = GeoSeries([Polygon([(e, n), (e, s), (w, s), (w, n)])])
