5f2b6b053980e6f628e92a7d914b3d254c893962
turnerm <turnermon@gmail.com>
2021-12-22 15:59:51 -0500

    Switched to scm versioning

--- src/aatoolbox/__init__.py
+++ src/aatoolbox/__init__.py
@@ -4,18 +4,4 @@ The ``aatoolbox`` library contains modules designed to assist with
 downloading and processing the data required for disaster-related
 anticipatory action.
 """
-from subprocess import CalledProcessError
-
-from aatoolbox.utils import git
-
-from ._version import __version__  # noqa: F401
-
-_MAIN_GIT_BRANCH = "main"
-
-# For development: add commit hash to version number to enable
-# upload of all builds to TestPyPI
-try:
-    if git.get_branch_name() != _MAIN_GIT_BRANCH:
-        __version__ += f".{git.get_short_hash()}"
-except CalledProcessError:
-    pass
+from ._version import version as __version__  # noqa: F401
