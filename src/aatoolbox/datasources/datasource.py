ed7362b03cc2cd457353ecf9d5baaac0cbe44775
Tinkaa <tinkavalentijn@gmail.com>
2022-03-17 13:29:55 +0100

    add is_global

--- src/aatoolbox/datasources/datasource.py
+++ src/aatoolbox/datasources/datasource.py
@@ -4,6 +4,8 @@ from pathlib import Path
 from aatoolbox.config.countryconfig import CountryConfig
 from aatoolbox.config.pathconfig import PathConfig
 
+_GLOBAL_DIR = "glb"
+
 
 class DataSource:
     """
@@ -25,19 +27,27 @@ class DataSource:
         country_config: CountryConfig,
         module_base_dir: str,
         is_public: bool = False,
+        is_global: bool = False,
     ):
 
         self._country_config = country_config
         self._module_base_dir = module_base_dir
         self._path_config = PathConfig()
         self._raw_base_dir = self._get_base_dir(
-            is_public=is_public, is_raw=True
+            is_public=is_public,
+            is_global=is_global,
+            is_raw=True,
         )
         self._processed_base_dir = self._get_base_dir(
-            is_public=is_public, is_raw=False
+            is_public=is_public, is_global=is_global, is_raw=False
         )
 
-    def _get_base_dir(self, is_public: bool, is_raw: bool) -> Path:
+    def _get_base_dir(
+        self,
+        is_public: bool,
+        is_raw: bool,
+        is_global: bool,
+    ) -> Path:
         permission_dir = (
             self._path_config.public
             if is_public
@@ -46,10 +56,13 @@ class DataSource:
         state_dir = (
             self._path_config.raw if is_raw else self._path_config.processed
         )
+        region_dir = (
+            self._country_config.iso3 if not is_global else _GLOBAL_DIR
+        )
         return (
             self._path_config.base_path
             / permission_dir
             / state_dir
-            / self._country_config.iso3
+            / region_dir
             / self._module_base_dir
         )
