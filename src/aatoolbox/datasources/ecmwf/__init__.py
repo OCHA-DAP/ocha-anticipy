13abfbce05e4809186840a9cce1ba6513baee9f6
Tinkaa <tinkavalentijn@gmail.com>
2022-09-16 15:07:47 +0200

    add init to ecmwf

---
+++ src/aatoolbox/datasources/ecmwf/__init__.py
@@ -0,0 +1,5 @@
+"""Methods pertaining to data provided by ECMWF.
+
+Thus far only the seasonal forecast has been implemented.
+The code here is still in an experimental state and should be improved.
+"""
