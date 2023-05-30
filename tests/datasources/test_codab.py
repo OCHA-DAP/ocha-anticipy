f0d7659f81f52cf7a012a7131495ef8ffa6fcf16
Seth Caldwell <caldwellst@gmail.com>
2023-05-30 09:27:44 +0100

    Fix CodAB tests

--- tests/datasources/test_codab.py
+++ tests/datasources/test_codab.py
@@ -29,14 +29,6 @@ def gpd_read_file(mocker):
     return mocker.patch("ochanticipy.datasources.codab.codab.gpd.read_file")
 
 
-class MockCustomLayer:
-    """Mock custom layers."""
-
-    def __init__(self, name, hdx_resource) -> None:
-        self.name = name
-        self.hdx_resource = hdx_resource
-
-
 def test_codab_download(mock_aa_data_dir, mock_country_config, downloader):
     """Test that load_codab calls the HDX API to download."""
     codab = CodAB(country_config=mock_country_config)
@@ -123,10 +115,8 @@ def test_codab_too_high_admin_level(mock_country_config):
 def test_codab_custom(mock_aa_data_dir, mock_country_config, gpd_read_file):
     """Test that load_codab_custom retrieves expected file and layer name."""
     custom_layer_number = 1
-    custom_layer_name_list = [
-        MockCustomLayer(name="custom_layer_A", hdx_resource=None),
-        MockCustomLayer(name="custom_layer_B", hdx_resource=None),
-    ]
+    custom_layer_name_list = ["custom_layer_A", "custom_layer_B"]
+
     mock_country_config.codab.custom_layer_names = custom_layer_name_list
     codab = CodAB(country_config=mock_country_config)
     codab.load_custom(custom_layer_number)
@@ -134,7 +124,7 @@ def test_codab_custom(mock_aa_data_dir, mock_country_config, gpd_read_file):
         f"zip://{mock_aa_data_dir}/public/raw/{mock_country_config.iso3}/"
         f"{DATASOURCE_BASE_DIR}/{mock_country_config.iso3}_"
         f"adm.shp.zip/"
-        f"{custom_layer_name_list[custom_layer_number].name}"
+        f"{custom_layer_name_list[custom_layer_number]}"
     )
 
 
